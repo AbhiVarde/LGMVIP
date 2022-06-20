cfcab3e7c2b632ad341172d61bc16e146c59b45f
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2022-06-20 14:25:10 +0530

    LGMVIP

---
+++ Task 2/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from "react";
+import NavBar from "./component/navBar.js"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+     {/*  */}
+     <NavBar/>
+    </div>
+  );
+}
+
+export default App;
